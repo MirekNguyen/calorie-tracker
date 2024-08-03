649a5efb5dde8821cfd72f8d3283c6622be123cf
Mirek Nguyen <mirek.nguyen@o2.cz>
2024-08-03 21:32:21 +0200

    feat: add client

---
+++ client/src/app/login/page.tsx
@@ -0,0 +1,19 @@
+'use client'
+
+import { useForm } from "react-hook-form";
+
+export default function Login() {
+  const form = useForm();
+  const { register } = form;
+  return (
+    <>
+      <form className="App">
+        <input type="text" />
+        <input type="email" />
+        <input type="password" />
+        <input type={"submit"}
+          style={{ backgroundColor: "#a1eafb" }} />
+      </form>
+    </>
+  );
+}
