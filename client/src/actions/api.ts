0feaf7a82e79ac637e2813146216298d3a4de067
Mirek Nguyen <mirek.nguyen@o2.cz>
2024-08-07 11:24:08 +0200

    feat(client): api url env and cross origin

--- client/src/actions/api.ts
+++ client/src/actions/api.ts
@@ -4,19 +4,14 @@ import Cookies from "js-cookie";
 
 const jwt = Cookies.get("jwt");
 export const api = axios.create({
-  baseURL: `${process.env.NEXT_PUBLIC_API_URL}/api`,
+  baseURL: `${process.env.NEXT_PUBLIC_API_URL}`,
   withCredentials: false,
   headers: {
     Authorization: "Bearer " + jwt,
-    'Access-Control-Allow-Origin': '*',
-
   },
 });
 
 export const baseApi = axios.create({
-  baseURL: `${process.env.NEXT_PUBLIC_API_URL}/api`,
+  baseURL: `${process.env.NEXT_PUBLIC_API_URL}`,
   withCredentials: false,
-  headers: {
-    'Access-Control-Allow-Origin': '*',
-  },
 });
