008fbd943f8ee5c0c1f7c421ea7f5e04b91ad8df
Mirek Nguyen <mirek.nguyen@o2.cz>
2024-08-09 17:24:55 +0200

    feat(client): custom meal post action

--- client/src/actions/meal-entry.ts
+++ client/src/actions/meal-entry.ts
@@ -1,10 +1,21 @@
-import { api } from "./api";
+import { CustomMealEntry } from '@/types/meal/customMealEntrySchema';
+import { api } from './api';
 
 export const submitMealEntry = async (mealId: number, amount: number) => {
-  const response = await api.post("meal-entry", {
+  const response = await api.post('meal-entry', {
     mealId,
     amount,
     date: new Date(),
+  });
+  return response;
+};
+export const submitCustomMealEntry = async (
+  customMealEntry: CustomMealEntry,
+) => {
+  const date = new Date();
+  const response = await api.post('meal-entry/custom', {
+    ...customMealEntry,
+    date
   });
   return response;
 };
