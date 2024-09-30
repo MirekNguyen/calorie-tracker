00da26ddb9bc1f58a1742ef12438b449dfadeaf2
Mirek Nguyen <mirek.nguyen@o2.cz>
2024-09-30 12:07:17 +0200

    refactor!: switch backend to nextjs

--- apps/server/seed.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-HOST=localhost
-POSTGRES_DB=calorie
-POSTGRES_USER=calorie
-POSTGRES_PASSWORD=calorie
-POSTGRES_PORT=5444
-set -ex
-
-export PG_PASSWORD="$(echo $POSTGRES_PASSWORD)" && psql -v ON_ERROR_STOP=1 -h "$HOST" --username "$POSTGRES_USER" --dbname "$POSTGRES_DB" -p "$POSTGRES_PORT" < seed.sql
--- apps/server/src/dto/date.type.ts
+++
@@ -1,6 +0,0 @@
-import { IsDate } from 'class-validator';
-
-export class DateType {
-  @IsDate()
-  date: Date;
-}
